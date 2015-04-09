47bd593c00469960ebf2932ff4dec1652b48a3a7
Thomas Hajcak <thomas.hajcak@gmail.com>
2015-04-08 20:07:24 -0400

    Initial Commit

---
+++ Trash Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trash Time
+//
+//  Created by Thomas Hajcak on 4/8/15.
+//  Copyright (c) 2015 Simple Ink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
